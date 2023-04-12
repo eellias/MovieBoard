995a12be004e1987a514b1c494581fbdbba03247
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2023-04-12 15:50:16 +0300

    Tab bar is made

---
+++ NetflixClone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadsViewController.swift
+//  NetflixClone
+//
+//  Created by Ilya Tovstokory on 12.04.2023.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by Ilya Tovstokory on 12.04.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
