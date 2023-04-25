5e407512de5e761b8a0d95a849b5bdab532764fe
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2023-04-25 14:44:45 +0300

    Connected to TMDB

---
+++ NetflixClone/Managers/APICaller.swift
@@ -0,0 +1,41 @@
+//
+//  APICaller.swift
+//  NetflixClone
+//
+//  Created by Ilya Tovstokory on 25.04.2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "9f8d6932fd381267abe26ce30bf50ce1"
+    static let baseURl = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedToGetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        
+        guard let url = URL(string: "\(Constants.baseURl)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
