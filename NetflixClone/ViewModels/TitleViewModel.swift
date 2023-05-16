a41b5d46ebce3f0857cf44e60324d9bde70196ea
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2023-05-16 17:14:22 +0300

    Upcoming page added

---
+++ NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by Ilya Tovstokory on 04.05.2023.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    
+    let titleName: String
+    let posterURL: String
+    
+}
